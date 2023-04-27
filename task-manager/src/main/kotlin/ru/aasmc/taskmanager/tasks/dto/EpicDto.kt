537244bf7e77ea6ee2a1f8d402570bc76a82612f
aleksas <test@test.com>
2023-04-27 12:52:56 +0300

    WIP: add dtos for task, subtask and epic

---
+++ task-manager/src/main/kotlin/ru/aasmc/taskmanager/tasks/dto/EpicDto.kt
@@ -0,0 +1,26 @@
+package ru.aasmc.taskmanager.tasks.dto
+
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.annotation.JsonProperty
+import ru.aasmc.taskmanager.tasks.model.TaskStatus
+import ru.aasmc.taskmanager.util.DateProcessor
+import java.time.LocalDateTime
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.PositiveOrZero
+
+data class EpicDto(
+    val id: Long? = null,
+    @NotEmpty
+    val name: String,
+    @NotEmpty
+    val description: String,
+    @JsonProperty("task_status", required = true)
+    val taskStatus: TaskStatus,
+    @field:PositiveOrZero
+    val duration: Long = 0,
+    @JsonProperty("start_time")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DateProcessor.DATE_FORMAT)
+    val startTime: LocalDateTime? = null,
+    @JsonProperty("subtask_ids")
+    val subtaskIds: List<Long>
+)
---
+++ task-manager/src/main/kotlin/ru/aasmc/taskmanager/tasks/dto/TaskDto.kt
@@ -0,0 +1,26 @@
+package ru.aasmc.taskmanager.tasks.dto
+
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+import ru.aasmc.taskmanager.tasks.model.TaskStatus
+import ru.aasmc.taskmanager.util.DateProcessor
+import java.time.LocalDateTime
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.Positive
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class TaskDto(
+    val id: Long? = null,
+    @NotEmpty
+    val name: String,
+    @NotEmpty
+    val description: String,
+    @JsonProperty("task_status", required = true)
+    val taskStatus: TaskStatus,
+    @field:Positive
+    val duration: Long = 0,
+    @JsonProperty("start_time", required = true)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DateProcessor.DATE_FORMAT)
+    val startTime: LocalDateTime
+)
